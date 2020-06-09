deca21386b329e313f3977c6f95688191e83883b
Oliver Levay <oliver.levay@hotmail.com>
2020-06-09 23:01:49 +0200

    Updates

--- precache-manifest.0bd87ab92af74705a031b73a51feef56.js
+++ precache-manifest.0bd87ab92af74705a031b73a51feef56.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3d4d2ceb2fa28f2d49fddba535fde4dd",
+    "revision": "5853bbba1dd1ca0c530cd10ff3d2f73e",
     "url": "/virtual-list/index.html"
   },
   {
-    "revision": "8b8a4ddef4a1dcdbebaf",
+    "revision": "dcb5bd0c16d1d5c5189c",
     "url": "/virtual-list/static/css/main.87002367.chunk.css"
   },
   {
-    "revision": "ec4978a99be23f224812",
-    "url": "/virtual-list/static/js/2.118134e9.chunk.js"
+    "revision": "a24e9e8dd1ebf9d81abf",
+    "url": "/virtual-list/static/js/2.aac3b79e.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/virtual-list/static/js/2.118134e9.chunk.js.LICENSE.txt"
+    "url": "/virtual-list/static/js/2.aac3b79e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8b8a4ddef4a1dcdbebaf",
-    "url": "/virtual-list/static/js/main.9f515820.chunk.js"
+    "revision": "dcb5bd0c16d1d5c5189c",
+    "url": "/virtual-list/static/js/main.f3f6bb28.chunk.js"
   },
   {
     "revision": "6e76421a5ccc4b508464",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/virtual-list/precache-manifest.b5ae4f7d747221ea2b59075278908db6.js"
+  "/virtual-list/precache-manifest.0bd87ab92af74705a031b73a51feef56.js"
 );
 
 self.addEventListener('message', (event) => {
